7639e2713ee219d54e5001b45849acc52784e00a
Dennis Hezel <dh@3yourmind.com>
2021-05-25 19:37:04 +0200

    test: Start adding benchmark

---
+++ tests/benchmark.cpp
@@ -0,0 +1,144 @@
+#include "cntgs/contiguous.h"
+#include "utils/string.h"
+
+#define ANKERL_NANOBENCH_IMPLEMENT
+#include <../third-party/nanobench.h>
+
+#include <memory_resource>
+#include <random>
+#include <vector>
+
+using namespace cntgs::test;
+
+using FixedSizeVector = cntgs::ContiguousVector<cntgs::FixedSize<float>>;
+using VaryingSizeVector = cntgs::ContiguousVector<cntgs::VaryingSize<float>>;
+
+static constexpr auto FLOAT_MAX = 100000.f;
+static std::random_device rd;
+static std::mt19937 gen(rd());
+static std::uniform_real_distribution<float> float_dist(0.f, FLOAT_MAX);
+
+auto work(float e) { return (e + std::sqrt(e)) * std::sqrt(e); }
+
+template <std::size_t N>
+void array_vs_cntgs(std::size_t elements, std::size_t fixed_size)
+{
+    std::vector<std::vector<float>> input{elements};
+    for (auto&& v : input)
+    {
+        v.resize(fixed_size);
+        std::generate_n(v.begin(), fixed_size, [&] { return float_dist(gen); });
+    }
+    std::vector<std::array<float, N>> array_vector{input.size()};
+    for (size_t i = 0; i < input.size(); i++)
+    {
+        std::copy(input[i].begin(), input[i].end(), array_vector[i].begin());
+    }
+    FixedSizeVector fixed_size_vector{input.size(), {fixed_size}};
+    for (size_t i = 0; i < input.size(); i++)
+    {
+        fixed_size_vector.emplace_back(input[i]);
+    }
+    VaryingSizeVector varying_size_vector{input.size(), input.size() * fixed_size * sizeof(float)};
+    for (size_t i = 0; i < input.size(); i++)
+    {
+        varying_size_vector.emplace_back(input[i]);
+    }
+    input.clear();
+    input.shrink_to_fit();
+    ankerl::nanobench::Bench().minEpochIterations(5).run(
+        format("std::array<{}> elements: {} fixed_size: {}", N, elements, fixed_size), [&] {
+            for (auto&& elem : array_vector)
+            {
+                for (size_t i = 0; i < fixed_size; i++)
+                {
+                    ankerl::nanobench::doNotOptimizeAway(work(elem[i]));
+                }
+            }
+        });
+    ankerl::nanobench::Bench().minEpochIterations(5).run(
+        format("cntgs::FixedSize elements: {} fixed_size: {}", elements, fixed_size), [&] {
+            for (auto&& [elem] : fixed_size_vector)
+            {
+                for (auto&& e : elem)
+                {
+                    ankerl::nanobench::doNotOptimizeAway(work(e));
+                }
+            }
+        });
+    ankerl::nanobench::Bench().minEpochIterations(5).run(
+        format("cntgs::VaryingSize elements: {} fixed_size: {}", elements, fixed_size), [&] {
+            for (auto&& [elem] : varying_size_vector)
+            {
+                for (auto&& e : elem)
+                {
+                    ankerl::nanobench::doNotOptimizeAway(work(e));
+                }
+            }
+        });
+}
+
+void vector_vs_cntgs(std::size_t elements, uint32_t variance)
+{
+    std::uniform_int_distribution<uint32_t> int_dist(0, variance);
+    std::vector<std::vector<float>> input{elements};
+    size_t total_size = 0;
+    for (auto&& v : input)
+    {
+        auto size = int_dist(gen);
+        v.resize(size);
+        total_size += v.size();
+        std::generate(v.begin(), v.end(), [&] { return float_dist(gen); });
+    }
+    std::pmr::monotonic_buffer_resource resource{total_size * sizeof(float) +
+                                                 elements * 16 * sizeof(std::pmr::vector<float>)};
+    std::pmr::vector<std::pmr::vector<float>> vector_vector{&resource};
+    vector_vector.reserve(input.size());
+    for (size_t i = 0; i < input.size(); i++)
+    {
+        vector_vector.emplace_back();
+    }
+    for (size_t i = 0; i < input.size(); i++)
+    {
+        vector_vector[i].assign(input[i].begin(), input[i].end());
+    }
+    VaryingSizeVector varying_size_vector{input.size(), total_size * sizeof(float)};
+    for (size_t i = 0; i < input.size(); i++)
+    {
+        varying_size_vector.emplace_back(input[i]);
+    }
+    input.clear();
+    input.shrink_to_fit();
+    ankerl::nanobench::Bench().run(format("std::vector<std::vector> elements: {} variance: 0-{}", elements, variance),
+                                   [&] {
+                                       for (auto&& elem : vector_vector)
+                                       {
+                                           for (auto&& e : elem)
+                                           {
+                                               ankerl::nanobench::doNotOptimizeAway(work(e));
+                                           }
+                                       }
+                                   });
+    ankerl::nanobench::Bench().run(format("cntgs::VaryingSize elements: {} variance: 0-{}", elements, variance), [&] {
+        for (auto&& [elem] : varying_size_vector)
+        {
+            for (auto&& e : elem)
+            {
+                ankerl::nanobench::doNotOptimizeAway(work(e));
+            }
+        }
+    });
+}
+
+int main()
+{
+    auto random = static_cast<uint32_t>(float_dist(gen) / FLOAT_MAX * 3);
+    array_vs_cntgs<15>(500000, random + 15);
+    array_vs_cntgs<30>(500000, random + 15);
+    array_vs_cntgs<45>(500000, random + 15);
+    array_vs_cntgs<100>(50000, random + 100);
+    array_vs_cntgs<200>(50000, random + 100);
+    vector_vs_cntgs(500000, random + 10);
+    vector_vs_cntgs(100000, random + 100);
+    vector_vs_cntgs(50000, random + 1000);
+}
--- tests/utils/string.h
+++ tests/utils/string.h
@@ -1,8 +1,33 @@
 #pragma once
 
+#include <sstream>
 #include <string>
+#include <string_view>
 
 namespace cntgs::test
 {
+namespace detail
+{
+template <class Iterator, class Arg>
+decltype(auto) format_one(std::ostringstream& stream, std::string_view format, Iterator& iterator, Arg&& arg)
+{
+    auto next = std::find(iterator, format.end(), '{');
+    auto* data = std::addressof(*iterator);
+    auto size = static_cast<std::size_t>(std::distance(iterator, next));
+    iterator = std::next(next, 2);
+    stream << std::string_view{data, size} << std::forward<Arg>(arg);
+    return stream;
+}
+}  // namespace detail
+
 size_t count_lines(const std::string& text);
+
+template <class... Args>
+auto format(std::string_view format, Args&&... args)
+{
+    std::ostringstream stream;
+    auto iterator = format.begin();
+    (detail::format_one(stream, format, iterator, std::forward<Args>(args)), ...);
+    return std::move(stream).str();
+}
 }  // namespace cntgs::test
