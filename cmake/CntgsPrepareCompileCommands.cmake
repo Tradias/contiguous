15cc994c6e1d610fd86ba2f0cb80921f9df2db4d
Dennis Hezel <dh@3yourmind.com>
2021-06-18 17:11:10 +0200

    chore: Re-enable sonarcloud scan including code coverage

---
+++ cmake/CntgsPrepareCompileCommands.cmake
@@ -0,0 +1,6 @@
+# args: WORKSPACE_ROOT
+
+set(NEW_WORKSPACE_ROOT_REGEX "\\/github\\/workspace\\/")
+string(REPLACE "/" "\\/" REGEX_WORKSPACE_ROOT "${WORKSPACE_ROOT}")
+execute_process(COMMAND sed -i "s/${REGEX_WORKSPACE_ROOT}/${NEW_WORKSPACE_ROOT_REGEX}/g"
+                        "${WORKSPACE_ROOT}/build-contiguous-debug/compile_commands.json")
