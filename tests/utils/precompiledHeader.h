01fb6c629564c9578dc91d2cd214bb9993e65416
Dennis Hezel <dh@3yourmind.com>
2021-07-04 14:22:22 +0200

    chore: Add precompiled header for test files

---
+++ tests/utils/precompiledHeader.h
@@ -0,0 +1,16 @@
+#include <algorithm>
+#include <array>
+#include <cstddef>
+#include <cstring>
+#include <iterator>
+#include <limits>
+#include <memory>
+#include <new>
+#include <tuple>
+#include <type_traits>
+#include <utility>
+#include <version>
+
+#ifdef __cpp_lib_concepts
+#include <concepts>
+#endif
