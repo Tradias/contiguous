c141bdbd5b3b8e380d04aca68713197777b1aefe
Dennis Hezel <dh@3yourmind.com>
2021-07-29 14:26:46 +0200

    chore: Clean up test coverage CMake target

--- cmake/CntgsCoverage.cmake
+++ cmake/CntgsCoverage.cmake
@@ -5,15 +5,13 @@ endfunction()
 
 function(cntgs_coverage_report_for_target _cntgs_target _cntgs_source)
     get_filename_component(_cntgs_source_name "${_cntgs_source}" NAME)
+    find_program(CNTGS_GCOV_PROGRAM gcov)
     add_custom_target(
         ${_cntgs_target}-coverage
-        COMMAND ${CMAKE_COMMAND} -E make_directory ${CNTGS_TEST_COVERAGE_OUTPUT_DIR}
-        DEPENDS ${_cntgs_target})
-    add_custom_command(
-        TARGET ${_cntgs_target}-coverage
+        DEPENDS ${_cntgs_target}
         COMMAND
-            gcov --relative-only --demangled-names --preserve-paths -o
-            $<TARGET_FILE_DIR:${_cntgs_target}>/CMakeFiles/${_cntgs_target}.dir/${_cntgs_source_name}.gcda
+            "${CNTGS_GCOV_PROGRAM}" --relative-only --demangled-names --preserve-paths -o
+            "$<TARGET_FILE_DIR:${_cntgs_target}>/CMakeFiles/${_cntgs_target}.dir/${_cntgs_source_name}.gcda"
             "${_cntgs_source}"
-        WORKING_DIRECTORY ${CMAKE_BINARY_DIR})
+        WORKING_DIRECTORY "${CMAKE_BINARY_DIR}")
 endfunction()
