91d0e3cd150cb3515723a5b70ef030158d37e63f
Dennis Hezel <dh@3yourmind.com>
2021-08-18 19:09:05 +0200

    chore: Fix coverage target after splitting test files

--- cmake/CntgsCoverage.cmake
+++ cmake/CntgsCoverage.cmake
@@ -3,15 +3,13 @@ function(cntgs_add_coverage_flags _cntgs_target)
     target_link_libraries(${_cntgs_target} PRIVATE gcov)
 endfunction()
 
-function(cntgs_coverage_report_for_target _cntgs_target _cntgs_source)
-    get_filename_component(_cntgs_source_name "${_cntgs_source}" NAME)
+function(cntgs_coverage_report_for_target _cntgs_target)
     find_program(CNTGS_GCOV_PROGRAM gcov)
     add_custom_target(
         ${_cntgs_target}-coverage
         DEPENDS ${_cntgs_target}
-        COMMAND
-            "${CNTGS_GCOV_PROGRAM}" --relative-only --demangled-names --preserve-paths -o
-            "$<TARGET_FILE_DIR:${_cntgs_target}>/CMakeFiles/${_cntgs_target}.dir/${_cntgs_source_name}.gcda"
-            "${_cntgs_source}"
-        WORKING_DIRECTORY "${CMAKE_BINARY_DIR}")
+        COMMAND "${CNTGS_GCOV_PROGRAM}" --relative-only --demangled-names --preserve-paths
+                $<TARGET_OBJECTS:${_cntgs_target}>
+        WORKING_DIRECTORY "${CMAKE_BINARY_DIR}"
+        COMMAND_EXPAND_LISTS)
 endfunction()
